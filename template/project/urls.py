8aacdc3bb8c4826dbc5c7c68f0eed70e86198be2
Jakob Blume <jakob.blume@mailbox.org>
2020-06-20 21:49:54 +0200

    Update urls.py

--- template/project/urls.py
+++ template/project/urls.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
 from django.urls import path, include
+
+
+def health(request: HttpRequest) -> HttpResponse:
+    from django.contrib.auth.models import User
+    from django.db.utils import OperationalError
+
+    try:
+        users = User.objects.all().first()
+    except OperationalError:
+        return HttpResponse(status=500)
+    else:
+        return HttpResponse(status=200)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
