7f15f8b7d6e6f718a6082c448a664e6b895e5992
Jakob Blume <jakob.blume@mailbox.org>
2020-06-20 21:52:56 +0200

    Add files via upload

---
+++ template/project/builders.py
@@ -0,0 +1,19 @@
+class ResponseDataBuilder:
+    @staticmethod
+    def build_error_response(
+        error_message: str, error_fields: dict = None, error_code: str = None
+    ) -> dict:
+        error_data = {"message": error_message}
+
+        if error_code is not None:
+            error_data.update({"code": error_code})
+        if error_fields is not None:
+            error_data.update({"fields": error_fields})
+
+        response = {"error": error_data}
+
+        return response
+
+    @staticmethod
+    def build_success_response(data: dict) -> dict:
+        return {"data": data}
